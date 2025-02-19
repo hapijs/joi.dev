aa977955c1164a42385daff38a27c12ba9a6e37f
Nicolas Morel <nicolas@morel.io>
2025-02-19 09:11:35 +0100

    chore: bump supported versions

--- cli/modules.js
+++ cli/modules.js
@@ -3,37 +3,37 @@ const modules = {
   joi: {
     package: 'joi',
     compatibility: {
-      17: ['14', '16', '18'],
+      17: ['14', '16', '18', '20', '22'],
     },
   },
   address: {
     package: '@hapi/address',
     compatibility: {
-      5: ['14', '16', '18'],
+      5: ['14', '16', '18', '20', '22'],
     },
   },
   formula: {
     package: '@hapi/formula',
     compatibility: {
-      3: ['14', '16', '18'],
+      3: ['14', '16', '18', '20', '22'],
     },
   },
   'joi-date': {
     package: '@joi/date',
     compatibility: {
-      2: ['14', '16', '18'],
+      2: ['14', '16', '18', '20', '22'],
     },
   },
   pinpoint: {
     package: '@hapi/pinpoint',
     compatibility: {
-      2: ['14', '16', '18'],
+      2: ['14', '16', '18', '20', '22'],
     },
   },
   tlds: {
     package: '@hapi/tlds',
     compatibility: {
-      1: ['14', '16', '18', '20'],
+      1: ['14', '16', '18', '20', '22'],
     },
   },
 };
