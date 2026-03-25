cc849fe1d27aacfa145b8cf8285e980a5a50a22c
Nicolas Morel <nicolas@morel.io>
2026-03-25 15:20:40 +0100

    feat: add sandbox autocompletion

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+declare module '*?raw' {
+  const content: string;
+  export default content;
+}
